5cf77b0444ab039350fead8b3520a9f137ed0491
Merlin <merlinosayimwen@gmail.com>
2019-06-06 22:34:22 +0200

    Extract Line from Position struct

--- pkg/source/position.go
+++ pkg/source/position.go
@@ -5,13 +5,31 @@ import "fmt"
 type LineIndex int16
 type Offset int16
 
+type Line struct {
+	Offset Offset
+	Length Offset
+	Index  LineIndex
+}
+
 type Position struct {
-	Offset     Offset
-	LineColumn Offset
-	LineIndex  LineIndex
+	Offset Offset
+	Column Offset
+	Line   Line
+}
+
+func (line Line) Contains(offset Offset) bool {
+	if line.Offset >= offset {
+		return true
+	}
+	return line.Offset+line.Length < offset
+}
+
+func (line Line) String() string {
+	return fmt.Sprintf("Line{index: %d, offset: %d, length:%d}",
+		line.Index, line.Offset, line.Length)
 }
 
 func (position Position) String() string {
-	return fmt.Sprintf("Position { Line: %d, Column: %d }",
-		position.LineIndex, position.LineColumn)
+	return fmt.Sprintf("Position{%d:%d}}",
+		position.Line, position.Column)
 }
