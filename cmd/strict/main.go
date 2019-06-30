a5ac014c7874af89a30e745a0eab1acfef2e29b4
Merlin <merlinosayimwen@gmail.com>
2019-06-30 18:11:52 +0200

    Add compile command
    Former-commit-id: 001185ba3c5d89cda91957b430f41419f16e9732

--- cmd/strict/main.go
+++ cmd/strict/main.go
@@ -1,3 +1,13 @@
-package strict
+package main
 
-func main() {}
+import "flag"
+
+var sourceFilePath string
+
+func init() {
+	flag.StringVar(&sourceFilePath, "source", "main.strict", "path to the source-file")
+}
+
+func main() {
+	flag.Parse()
+}
