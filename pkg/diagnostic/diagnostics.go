ab228bf42eacdad4bdd5b80288aac7ae48b0a84a
Merlin <merlinosayimwen@gmail.com>
2019-07-01 10:41:16 +0200

    Implement diagnostics.Recorder
    Former-commit-id: d7dbbceed84944e4e9b560089dc81877a12fe178

--- pkg/diagnostic/diagnostics.go
+++ pkg/diagnostic/diagnostics.go
@@ -1,6 +1,7 @@
 package diagnostic
 
 type Recorder struct {
+	entries map[string][]Entry
 }
 
 func NewRecorder() *Recorder {
@@ -8,5 +9,34 @@ func NewRecorder() *Recorder {
 }
 
 func (recorder *Recorder) Record(entry Entry) {
+	recorder.appendToKind(entry.Kind, entry)
+}
 
+func (recorder *Recorder) entriesOfKind(kind *Kind) ([]Entry, bool) {
+	entries, ok := recorder.entries[kind.Name]
+	return entries, ok
+}
+
+func (recorder *Recorder) appendToKind(kind *Kind, entry Entry) {
+	recorder.entries[kind.Name] = append(recorder.entries[kind.Name], entry)
+}
+
+func (recorder *Recorder) Entries(kind *Kind) ([]Entry, bool) {
+	return recorder.entriesOfKind(kind)
+}
+
+func (recorder *Recorder) AllEntries() []Entry {
+	var all []Entry
+	for _, entries := range recorder.entries {
+		all = append(all, entries...)
+	}
+	return all
+}
+
+func (recorder *Recorder) PrintAllEntries(printer Printer) {
+	for _, entries := range recorder.entries {
+		for _, entry := range entries {
+			entry.PrintColored(printer)
+		}
+	}
 }
