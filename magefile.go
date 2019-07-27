ee47cce067bf4700eb181f5a24bd04ce869e0f1c
Merlin Osayimwen <merlinosayimwen@gmail.com>
2019-07-27 21:00:56 +0200

    Use mage build-tool to build sdk
    Former-commit-id: 9184d1f5ad386cf36d805c220df801507b892395

---
+++ magefile.go
@@ -0,0 +1,62 @@
+// +build mage
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/magefile/mage/mg"
+)
+
+const BinaryName = "strict"
+
+func Build() error {
+	mg.Deps(InstallDeps)
+	cmd := exec.Command("go", "build", "-o", BinaryName, "./cmd/strict")
+	return cmd.Run()
+}
+
+func Install() error {
+	mg.Deps(Build)
+	command := exec.Command("go", "install", ".cmd/strict")
+	return command.Run()
+}
+
+func InstallDeps() error {
+	cmd := exec.Command("glide")
+	return cmd.Run()
+}
+
+func Clean() (err error) {
+	if err = runGoClean(); err != nil {
+		return
+	}
+	if err = deleteDirectory(pathToBinary()); err != nil {
+		return
+	}
+	return nil
+}
+
+func deleteDirectory(path string) error {
+	command := exec.Command("rm", "-rf", path)
+	return command.Run()
+}
+
+func runGoClean() error {
+	command := exec.Command("go", "clean")
+	return command.Run()
+}
+
+func pathToBinary() string {
+	goBin := os.Getenv("GOBIN")
+	if goBin == "" {
+		goPath := os.Getenv("GOPATH")
+		if goPath == "" {
+			mg.Fatal(1, "GOPATH or GOBIN not set")
+			panic("no GOPATH or GOBIN set")
+		}
+	}
+	return fmt.Sprintf("%s/%s", goBin, BinaryName)
+}
