bcf99cd6dcd30300d4fe9957f20134af9b659435
Merlin Osayimwen <merlinosayimwen@gmail.com>
2019-10-11 02:35:46 +0200

    Add tests for typename nodes
    Former-commit-id: 8bc704ccac2a36160c31e2e15c741b853d600598

---
+++ pkg/compiler/grammar/tree/concrete_type_name.go
@@ -0,0 +1,29 @@
+package tree
+
+import "gitlab.com/strict-lang/sdk/pkg/compiler/input"
+
+type ConcreteTypeName struct {
+	Name   string
+	Region input.Region
+}
+
+func (concrete *ConcreteTypeName) NonGenericName() string {
+	return concrete.Name
+}
+
+func (concrete *ConcreteTypeName) FullName() string {
+	return concrete.Name
+}
+
+func (concrete *ConcreteTypeName) Accept(visitor Visitor) {
+	visitor.VisitConcreteTypeName(concrete)
+}
+
+func (concrete *ConcreteTypeName) AcceptRecursive(visitor Visitor) {
+	concrete.Accept(visitor)
+}
+
+func (concrete *ConcreteTypeName) Locate() input.Region {
+	return concrete.Region
+}
+
---
+++ pkg/compiler/grammar/tree/concrete_type_name_test.go
@@ -0,0 +1,39 @@
+package tree
+
+import (
+	"gitlab.com/strict-lang/sdk/pkg/compiler/input"
+	"testing"
+)
+
+func TestConcreteTypeName_Accept(testing *testing.T) {
+	entry := &ConcreteTypeName{}
+	CreateVisitorTest(entry, testing).Expect(ConcreteTypeNameNodeKind).Run()
+}
+
+func TestConcreteTypeName_AcceptRecursive(testing *testing.T) {
+	entry := &ConcreteTypeName{}
+	CreateVisitorTest(entry, testing).Expect(ConcreteTypeNameNodeKind).RunRecursive()
+}
+
+func TestConcreteTypeName_Locate(testing *testing.T) {
+	RunNodeRegionTest(testing, func(region input.Region) Node {
+		return &ConcreteTypeName{
+			Name:   "type",
+			Region: region,
+		}
+	})
+}
+
+func TestConcreteTypeName_NonGenericName(testing *testing.T) {
+	entries := []*ConcreteTypeName{
+		{Name: "SomethingConcrete"},
+		{Name: "ThisIsTheNameOfAType"},
+		{Name: "int"},
+	}
+	for _, entry := range entries {
+		if entry.NonGenericName() != entry.Name {
+			testing.Errorf("Entry has invalid NonGenericName(): expected %s - got %s",
+				entry.NonGenericName(), entry.Name)
+		}
+	}
+}
