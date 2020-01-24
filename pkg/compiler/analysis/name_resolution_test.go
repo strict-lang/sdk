36bf53870679f2e776ce4acfd251a461e826cc8b
Merlin Osayimwen <merlinosayimwen@gmail.com>
2020-01-24 21:27:02 +0100

    Fix failing tests
    Former-commit-id: 838fe2c6214e09c5b7de2a6b83b63dcb308cf5e4

--- pkg/compiler/analysis/name_resolution_test.go
+++ pkg/compiler/analysis/name_resolution_test.go
@@ -1,29 +1,32 @@
 package analysis
 
 import (
-	"fmt"
 	"gitlab.com/strict-lang/sdk/pkg/compiler/diagnostic"
 	"gitlab.com/strict-lang/sdk/pkg/compiler/grammar/syntax"
+	"gitlab.com/strict-lang/sdk/pkg/compiler/grammar/tree"
 	"gitlab.com/strict-lang/sdk/pkg/compiler/isolate"
 	passes "gitlab.com/strict-lang/sdk/pkg/compiler/pass"
 	"testing"
 )
 
-var testUnit = syntax.ParseString("Test",
-	`
+func parseTestUnit() *tree.TranslationUnit {
+	result := syntax.ParseString("Test",
+		`
 method Number add(Number left, Number right)
   return left + right
 
 method Number addPositive(Number left, Number right)
-  if left < 0 or right < 0 do
+  if left < 0 or right < 0
     return 0
   return add(left, right)
 
-`).TranslationUnit
+`)
+	return result.TranslationUnit
+}
 
 func TestNameResolutionPass(testing *testing.T) {
 	context := &passes.Context{
-		Unit:       testUnit,
+		Unit:       parseTestUnit(),
 		Diagnostic: diagnostic.NewBag(),
 		Isolate:    isolate.SingleThreaded(),
 	}
@@ -31,5 +34,4 @@ func TestNameResolutionPass(testing *testing.T) {
 	if err := execution.Run(); err != nil {
 		testing.Error(err)
 	}
-	fmt.Printf("%#v\n",context.Unit)
 }
